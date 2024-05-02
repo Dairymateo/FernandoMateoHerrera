475f84bc0abc1067df0d525942e439f4ebd19b66
Dairymateo <seoxyt15@gmail.com>
2024-05-02 07:24:36 -0400

    Add project files.

---
+++ FernandoMateoHerrera/Models/Carrera.cs
@@ -0,0 +1,12 @@
+﻿namespace FernandoMateoHerrera.Models
+{
+    public class Carrera
+
+    {
+        public int CarreraId { get; set; }
+        public String NombreCarrera { get; set; }
+        public String Campus {  get; set; }
+        public int Semestre { get; set; }
+
+    }
+}
---
+++ FernandoMateoHerrera/Models/MHerrera.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FernandoMateoHerrera.Models
+{
+    public class MHerrera
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+
+        public Boolean Ecuatoriano { get; set; }
+        [AllowNull]
+        public double TimpoPromedio { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        [ForeignKey("CareraId")]
+        public int? CarreraId { get; set; }
+        public Carrera? Carrera { get; set; }
+    }
+}
