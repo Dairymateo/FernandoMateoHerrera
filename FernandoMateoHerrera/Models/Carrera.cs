d673000872c1508aa2eff7cb313bcbb8711b1cf7
Dairymateo <seoxyt15@gmail.com>
2024-05-02 07:59:39 -0400

    Agregar estilo a la pagina

--- FernandoMateoHerrera/Models/Carrera.cs
+++ FernandoMateoHerrera/Models/Carrera.cs
@@ -1,8 +1,11 @@
-﻿namespace FernandoMateoHerrera.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FernandoMateoHerrera.Models
 {
     public class Carrera
 
     {
+        [Key]
         public int CarreraId { get; set; }
         public String NombreCarrera { get; set; }
         public String Campus {  get; set; }
